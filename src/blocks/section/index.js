a4409eaf2ff11ccdd9f538a70b1167bdc768f0aa
Rhonalf Martinez <rhonalf.martinez@koombea.com>
2019-12-30 15:05:08 -0500

    section block demo

---
+++ src/blocks/section/index.js
@@ -0,0 +1,247 @@
+import './editor.scss';
+
+import blockAttributes from './attributes';
+import { Fragment } from 'react';
+
+const { __ } = wp.i18n;
+const { registerBlockType } = wp.blocks;
+const {
+  InspectorControls,
+  InnerBlocks,
+  withColors,
+  PanelColorSettings,
+  getColorClassName,
+  MediaUploadCheck,
+  MediaUpload
+} = wp.blockEditor;
+const {
+  PanelBody,
+  PanelRow,
+  ToggleControl,
+  RangeControl,
+  Button
+} = wp.components;
+
+// const editBlock = function (props) {
+//   const { attributes: { content }, setAttributes, className } = props;
+//   const onChangeContent = (newContent) => {
+//     setAttributes({ content: newContent });
+//   };
+//   return (
+//     <RichText
+//       tagName="p"
+//       className={className}
+//       onChange={onChangeContent}
+//       value={content}
+//     />
+//   );
+// };
+const editBlock = function (props) {
+  return <Fragment>
+    <InnerBlocks></InnerBlocks>
+    <InspectorControls>
+      <PanelBody title={__('Section settings', 'kili-builder')}>
+        <PanelRow>
+          <ToggleControl
+            label={__('Use container', 'kili-builder')}
+            onChange={(value) => { props.setAttributes({ useContainer: value }); }}
+            checked={props.attributes.useContainer}/>
+        </PanelRow>
+        <PanelRow className="kili-section__component-item">
+          {props.attributes.mobileBackgroundImage ?
+            <div className="components-base-control">
+              <img src={props.attributes.mobileBackgroundImage} className="kili-section__placeholder-image"/>
+            </div>
+            : <p>{__('No image has been set yet', 'kili-builder')}</p>}
+          <MediaUploadCheck>
+            <MediaUpload
+              onSelect={(media) => { props.setAttributes({ mobileBackgroundImage: media.url, mobileBackgroundImageId: media.id }); }}
+              allowedTypes={['image']}
+              value={props.attributes.mobileBackgroundImageId}
+              render={({ open }) => (
+                <Button onClick={open}>{__('Mobile background image', 'kili-builder')}</Button>
+              )}
+            />
+          </MediaUploadCheck>
+        </PanelRow>
+        <PanelRow className="kili-section__component-item">
+          {props.attributes.backgroundImage ?
+            <div className="components-base-control">
+              <img src={props.attributes.backgroundImage} className="kili-section__placeholder-image" />
+            </div>
+            : <p>{__('No image has been set yet', 'kili-builder')}</p>}
+          <MediaUploadCheck>
+            <MediaUpload
+              onSelect={(media) => { props.setAttributes({ backgroundImage: media.url, backgroundImageId: media.id }); }}
+              allowedTypes={['image']}
+              value={props.attributes.backgroundImage}
+              render={({ open }) => (
+                <Button onClick={open}>{__('Desktop background image', 'kili-builder')}</Button>
+              )}
+            />
+          </MediaUploadCheck>
+        </PanelRow>
+      </PanelBody>
+      <PanelColorSettings
+        title={__('Background color', 'kili-builder')}
+        colorSettings={[
+          {
+            label: __('Choose color', 'kili-builder'),
+            value: props.attributes.backgroundColor,
+            onChange: (value) => { props.setAttributes({ backgroundColor: value }); }
+          }
+        ]}/>
+      <PanelBody title={__('Mobile settings', 'kili-builder')} initialOpen={false}>
+        <PanelRow className="kili-section__component-item">
+          <RangeControl
+            label={__('Margin bottom (in px)', 'kili-builder')}
+            onChange={(value) => { props.setAttributes({ marginBottomMobile: value }); }}
+            value={props.attributes.marginBottomMobile}
+            min={0}
+            max={100} />
+        </PanelRow>
+        <PanelRow className="kili-section__component-item">
+          <RangeControl
+            label={__('Margin top (in px)', 'kili-builder')}
+            onChange={(value) => { props.setAttributes({ marginTopMobile: value }); }}
+            value={props.attributes.marginTopMobile}
+            min={0}
+            max={100} />
+        </PanelRow>
+        <PanelRow className="kili-section__component-item">
+          <RangeControl
+            label={__('Padding bottom (in px)', 'kili-builder')}
+            onChange={(value) => { props.setAttributes({ paddingBottomMobile: value }); }}
+            value={props.attributes.paddingBottomMobile}
+            min={0}
+            max={100} />
+        </PanelRow>
+        <PanelRow className="kili-section__component-item">
+          <RangeControl
+            label={__('Padding top (in px)', 'kili-builder')}
+            onChange={(value) => { props.setAttributes({ paddingTopMobile: value }); }}
+            value={props.attributes.paddingTopMobile}
+            min={0}
+            max={100} />
+        </PanelRow>
+      </PanelBody>
+      <PanelBody title={__('Desktop settings', 'kili-builder')} initialOpen={false}>
+        <PanelRow className="kili-section__component-item">
+          <RangeControl
+            label={__('Margin bottom (in px)', 'kili-builder')}
+            onChange={(value) => { props.setAttributes({ marginBottomDesktop: value }); }}
+            value={props.attributes.marginBottomDesktop}
+            min={0}
+            max={100} />
+        </PanelRow>
+        <PanelRow className="kili-section__component-item">
+          <RangeControl
+            label={__('Margin top (in px)', 'kili-builder')}
+            onChange={(value) => { props.setAttributes({ marginTopDesktop: value }); }}
+            value={props.attributes.marginTopDesktop}
+            min={0}
+            max={100} />
+        </PanelRow>
+        <PanelRow className="kili-section__component-item">
+          <RangeControl
+            label={__('Padding bottom (in px)', 'kili-builder')}
+            onChange={(value) => { props.setAttributes({ paddingBottomDesktop: value }); }}
+            value={props.attributes.paddingBottomDesktop}
+            min={0}
+            max={100} />
+        </PanelRow>
+        <PanelRow className="kili-section__component-item">
+          <RangeControl
+            label={__('Padding top (in px)', 'kili-builder')}
+            onChange={(value) => { props.setAttributes({ paddingTopDesktop: value }); }}
+            value={props.attributes.paddingTopDesktop}
+            min={0}
+            max={100} />
+        </PanelRow>
+      </PanelBody>
+    </InspectorControls>
+  </Fragment>;
+};
+
+const processStyles = function (props) {
+  let styles = '';
+  const customClassName = '.kili-section-' + props.attributes.UUID;
+  if (props.attributes.backgroundColor) {
+    styles += customClassName + '{background-color:' + props.attributes.backgroundColor + ';';
+  }
+  if (props.attributes.paddingTopMobile) {
+    styles += (styles !== '' ? '' : customClassName + '{') +
+      'padding-top:' + props.attributes.paddingTopMobile + 'px;';
+  }
+  if (props.attributes.paddingBottomMobile) {
+    styles += (styles !== '' ? '' : customClassName + '{') +
+      'padding-bottom:' + props.attributes.paddingBottomMobile + 'px;';
+  }
+  if (props.attributes.marginTopMobile) {
+    styles += (styles !== '' ? '' : customClassName + '{') +
+      'margin-top:' + props.attributes.marginTopMobile + 'px;';
+  }
+  if (props.attributes.marginBottomMobile) {
+    styles += (styles !== '' ? '' : customClassName + '{') +
+      'margin-bottom:' + props.attributes.marginBottomMobile + 'px;';
+  }
+  if (styles !== '') {
+    styles += '}';
+  }
+  if (props.attributes.paddingBottomDesktop
+    || props.attributes.paddingTopDesktop
+    || props.attributes.marginTopDesktop
+    || props.attributes.marginBottomDesktop) {
+    let deskStyle = '';
+    if (props.attributes.paddingBottomDesktop) {
+      deskStyle = customClassName + '{padding-bottom:' + props.attributes.paddingBottomDesktop + 'px;'
+    }
+    if (props.attributes.paddingTopDesktop) {
+      deskStyle += (deskStyle === '' ? customClassName + '{' : '');
+      deskStyle += 'padding-top:' + props.attributes.paddingTopDesktop + 'px;';
+    }
+    if (props.attributes.marginBottomDesktop) {
+      deskStyle = customClassName + '{margin-bottom:' + props.attributes.marginBottomDesktop + 'px;'
+    }
+    if (props.attributes.marginTopDesktop) {
+      deskStyle += (deskStyle === '' ? customClassName + '{' : '');
+      deskStyle += 'margin-top:' + props.attributes.marginTopDesktop + 'px;';
+    }
+    if (deskStyle !== '') {
+      deskStyle += '}'
+      styles += '@media all and (min-width:815px){' + deskStyle + '}';
+    }
+  }
+  return styles;
+};
+
+const saveBlock = function (props) {
+  let styles = processStyles(props);
+  let classNameString = typeof props.className ? '' : props.className;
+  if (typeof props.attributes.UUID !== 'undefined') {
+    classNameString += ' kili-section-' + props.attributes.UUID;
+  }
+
+  classNameString = classNameString.replace('undefined', '');
+  classNameString = classNameString.replace(/\s{2,}/g, ' ');
+
+  return <section className={classNameString}>
+    <div className={props.attributes.useContainer ? 'container' : ''}>
+      <InnerBlocks.Content />
+    </div>
+    {styles !== '' ? <style>{styles}</style> : ''}
+  </section>;
+};
+
+registerBlockType( 'kili/section', {
+  attributes: blockAttributes,
+  category: 'layout',
+  icon: 'editor-insertmore',
+  edit: editBlock,
+  save: saveBlock,
+  supports: {
+    align: ['wide', 'full'],
+  },
+  title: __('Kili Section', 'kili-builder'),
+  description: __('Creates a section element', 'kili-builder'),
+} );
--- src/init.php
+++ src/init.php
@@ -13,6 +13,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+function get_scripts_array() {
+	return array(
+		[
+			'block_id' => 'kili/section',
+			'editor_script_key' => 'kili/kili-section-editor-script',
+			'name' => __( 'Section', 'kili-builder' ),
+		],
+	);
+}
+
 /**
  * Enqueue Gutenberg block assets for both frontend + backend.
  *
@@ -30,60 +40,33 @@ if ( ! defined( 'ABSPATH' ) ) {
 function kili_builder_cgb_block_assets() { // phpcs:ignore
 	// Register block styles for both frontend + backend.
 	wp_register_style(
-		'kili_builder-cgb-style-css', // Handle.
-		plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ ) ), // Block style CSS.
-		array( 'wp-editor' ), // Dependency to include the CSS after it.
-		null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: File modification time.
+		'kili-blocks-style',
+		plugins_url( 'dist/main.css', dirname( __FILE__ ) ),
+		array( 'wp-edit-blocks' )
 	);
 
-	// Register block editor script for backend.
-	wp_register_script(
-		'kili_builder-cgb-block-js', // Handle.
-		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
-		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ), // Dependencies, defined above.
-		null, // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
-		true // Enqueue the script in the footer.
-	);
-
-	// Register block editor styles for backend.
-	wp_register_style(
-		'kili_builder-cgb-block-editor-css', // Handle.
-		plugins_url( 'dist/blocks.editor.build.css', dirname( __FILE__ ) ), // Block editor CSS.
-		array( 'wp-edit-blocks' ), // Dependency to include the CSS after it.
-		null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: File modification time.
-	);
-
-	// WP Localized globals. Use dynamic PHP stuff in JavaScript via `cgbGlobal` object.
-	wp_localize_script(
-		'kili_builder-cgb-block-js',
-		'cgbGlobal', // Array containing dynamic data for a JS Global.
-		[
-			'pluginDirPath' => plugin_dir_path( __DIR__ ),
-			'pluginDirUrl'  => plugin_dir_url( __DIR__ ),
-			// Add more data here that you want to access from `cgbGlobal` object.
-		]
-	);
-
-	/**
-	 * Register Gutenberg block on server-side.
-	 *
-	 * Register the block on server-side to ensure that the block
-	 * scripts and styles for both frontend and backend are
-	 * enqueued when the editor loads.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/blocks/writing-your-first-block-type#enqueuing-block-scripts
-	 * @since 1.16.0
-	 */
-	register_block_type(
-		'cgb/block-kili-builder', array(
-			// Enqueue blocks.style.build.css on both frontend & backend.
-			'style'         => 'kili_builder-cgb-style-css',
-			// Enqueue blocks.build.js in the editor only.
-			'editor_script' => 'kili_builder-cgb-block-js',
-			// Enqueue blocks.editor.build.css in the editor only.
-			'editor_style'  => 'kili_builder-cgb-block-editor-css',
-		)
-	);
+	$scripts_array = get_scripts_array();
+	foreach ($scripts_array as $script) {
+		wp_register_script(
+			$script['editor_script_key'],
+			plugins_url( 'dist/blocks.js', dirname( __FILE__ ) ),
+			array(
+				'wp-blocks',
+				'wp-i18n',
+				'wp-element',
+				'wp-block-editor',
+				'wp-components',
+				'wp-compose',
+			)
+		);
+		register_block_type(
+			$script['block_id'],
+			array(
+				'editor_script' => $script['editor_script_key'],
+				'editor_style' => 'kili-blocks-style',
+			)
+		);
+	}
 }
 
 // Hook: Block assets.

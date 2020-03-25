c0eb1438629ab84a3d76806cdebcfedfdf786284
Juan Estrada <juan.estrada@koombea.com>
2020-03-25 09:07:16 -0500

    chore: remove unused attribute

--- src/blocks/carousel/attributes.js
+++ src/blocks/carousel/attributes.js
@@ -51,34 +51,6 @@ export default {
       },
     },
   },
-  slickSettings: {
-    type: 'object',
-    default: {
-      dots: true,
-      arrows: false,
-      infinite: false,
-      speed: 300,
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            infinite: true,
-          },
-        }, {
-          breakpoint: 640,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: true,
-          },
-        },
-      ],
-    },
-  },
   slidesToShow: defaultAttrBuiler(null, {
     [DESKTOP]: 3,
     [TABLET]: 2,
